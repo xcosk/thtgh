31f384e656bdb40a901c5ce7fd2ee0e1156f4a3e
xcosk <maks.m90.00@mail.ru>
2024-09-12 16:28:40 +0300

    upi

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  isAdult = true
+  
+  return (
+    <>
+    <h1>{isAdult ? 'da' : 'net'}</h1>
+    </>
+  )
+}
+
+export default App
